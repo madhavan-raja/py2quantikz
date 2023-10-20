7061d998348ce54ecb023c71fc5414eed0aa1916
Madhavan Raja <madhavanraja99@gmail.com>
2023-10-20 11:04:07 -0700

    Uploaded version 0.0.2 to PyPI

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Converts Quantum Circuit Code to quantikz Diagrams'
+LONG_DESCRIPTION = open('README.md').read()
+
+print(LONG_DESCRIPTION)
 
 setup(
     name='py2quantikz',
     version=VERSION,
+    license='MIT',
     author='Madhavan Raja',
     author_email='madhavanraja99@gmail.com',
     description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/madhavan-raja/py2quantikz',
     packages=find_packages(),
+    keywords=['python', 'tikz', 'quantikz', 'latex', 'quantum'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
